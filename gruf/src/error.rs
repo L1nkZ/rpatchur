c7cb87cf46653e19a86550af30367fe4114a1fb2
LinkZ <wanthost@gmail.com>
2020-11-11 16:43:33 +0100

    Implement custom result and error types for gruf
    Use it as the default returned result
    Run cargo update

---
+++ gruf/src/error.rs
@@ -0,0 +1,33 @@
+use std::io;
+
+use thiserror::Error;
+
+pub type Result<T> = std::result::Result<T, GrufError>;
+
+#[derive(Error, Debug)]
+pub enum GrufError {
+    #[error("io error: {0}")]
+    IoError(#[from] io::Error),
+    #[error("failed to parse archive: {0}")]
+    ParsingError(String),
+    #[error("failed to find file entry")]
+    EntryNotFound,
+    #[error("failed to read content: {0}")]
+    InvalidContent(String),
+    #[error("failed to serialize data: {0}")]
+    SerializationError(String),
+}
+
+impl GrufError {
+    pub fn parsing_error(msg: impl Into<String>) -> Self {
+        Self::ParsingError(msg.into())
+    }
+
+    pub fn invalid_content(msg: impl Into<String>) -> Self {
+        Self::InvalidContent(msg.into())
+    }
+
+    pub fn serialization_error(msg: impl Into<String>) -> Self {
+        Self::SerializationError(msg.into())
+    }
+}
