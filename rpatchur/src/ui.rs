be9d667a892da11241c0e7ed5b75a807efc345e4
Vincent Stumpf <vincents.995@gmail.com>
2021-03-12 21:59:24 -0800

    address comments, remove quotes from arguments

--- rpatchur/src/ui.rs
+++ rpatchur/src/ui.rs
@@ -250,11 +250,20 @@ fn handle_login(webview: &mut WebView<WebViewUserData>, parameters: Value) {
     match result {
         Err(e) => log::error!("Invalid arguments given for 'login': {}", e),
         Ok(login_params) => {
-            let mut play_arguments: Vec<String> = Vec::new();
             // Push credentials to the list of arguments first
-            play_arguments.push(format!("-t:{}", login_params.password));
-            play_arguments.push(login_params.login);
-            play_arguments.extend(webview.user_data().patcher_config.play.arguments.clone());
+            let mut play_arguments: Vec<String> = vec![
+                format!("-t:{}", login_params.password),
+                login_params.login,
+                "server".to_string(),
+            ];
+            play_arguments.extend(
+                webview
+                .user_data()
+                .patcher_config
+                .play
+                .arguments
+                .clone()
+            );
             start_game_client(webview, &play_arguments);
         }
     }
